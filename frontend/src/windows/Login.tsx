2879e9da17b9bd7877dc9c445cb1d0dda145bfc9
CourtneyThan <55212944+CourtneyThan@users.noreply.github.com>
2023-02-17 15:16:41 -0500

    Courtneys Courtneys

--- frontend/src/windows/Login.tsx
+++ frontend/src/windows/Login.tsx
@@ -4,12 +4,12 @@ import Navbar from '../components/navbar';
 import '../css/LoginRegister.css';
 
 export default function Login() {
-    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
+        console.log(username);
     }
     
     return (
@@ -17,8 +17,8 @@ export default function Login() {
             <Navbar/>
         <div className='form-container' id='login-register-background'>
             <form className='login-form' onSubmit={handleSubmit}>
-                <label htmlFor="email">Email</label>
-                <input type="email" placeholder="emailname@gmail.com" id="email" name="email"></input>
+                <label htmlFor="username">Username</label>
+                <input type="username" placeholder="John Smith" id="username" name="username"></input>
                 <label htmlFor="password">Password</label>
                 <input type="password" placeholder="enter your password" id="password" name="password"></input>
                 <button type="button">Login</button>
