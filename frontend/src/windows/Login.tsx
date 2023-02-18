decfc2d07bdd2301e19c4bcff6c8d8fb4eae7efc
Juan Gutierrez <juan.gutierrez0157@gmail.com>
2023-02-17 21:52:04 -0500

    Add login API call to login form

--- frontend/src/windows/Login.tsx
+++ frontend/src/windows/Login.tsx
@@ -1,30 +1,43 @@
 import React, { useState } from 'react';
 import Navbar from '../components/navbar';
+import * as API from '../functions/API';
 
 import '../css/LoginRegister.css';
 
 export default function Login() {
-    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
-    }
-    
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        const response = await API.login({username, password});
+        const responseBody = await response.json();
+        
+        if (!response.ok) {
+            console.log(responseBody.message);
+            return;
+        }
+
+        localStorage.setItem("synesthizeUserData", JSON.stringify({
+            userId: responseBody._id,
+            username: responseBody.username,
+        }));
+    };
+
     return (
         <div>
             <Navbar/>
-        <div className='form-container' id='login-register-background'>
-            <form className='login-form' onSubmit={handleSubmit}>
-                <label htmlFor="email">Email</label>
-                <input type="email" placeholder="emailname@gmail.com" id="email" name="email"></input>
-                <label htmlFor="password">Password</label>
-                <input type="password" placeholder="enter your password" id="password" name="password"></input>
-                <button type="button">Login</button>
-            </form> <br />
-            <button className='linkToNextPage' onClick={() => window.location.href='/register'}>Register new account</button>
-        </div>
+            <div className='form-container' id='login-register-background'>
+                <form className='login-form' onSubmit={handleSubmit}>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" placeholder="enter your username" id="username" name="username" onChange={(e) => setUsername(e.target.value)}></input>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" placeholder="enter your password" id="password" name="password" onChange={(e) => setPassword(e.target.value)}></input>
+                    <button type="submit">Login</button>
+                </form> <br />
+                <button className='linkToNextPage' onClick={() => window.location.href='/register'}>Register new account</button>
+            </div>
         </div>
     )
 }
